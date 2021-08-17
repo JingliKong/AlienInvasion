4cc0456840b0cca86620557a7c34a24a1d643fef
JingliKong <alpha282828@gmail.com>
2021-08-17 03:04:08 -0500

    Ship Moves Left and Right

---
+++ settings.py
@@ -0,0 +1,12 @@
+class Settings:
+    """A class to store all settings for Alien Invasion"""
+
+    def __init__(self):
+        """Initializes the game's settings."""
+        # Screen Settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        #self.bg_color = (230, 230, 230)
+        self.bg_color = (0, 0, 0)
+        #ship settings 
+        self.ship_speed = 1.5 
